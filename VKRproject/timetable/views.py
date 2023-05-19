1cd7fb1a68ad15ac2149284cae6e86475caea639
Мартюгов Сергей <ceregamart2003@gmail.com>
2023-05-19 17:21:47 +0300

    изменён пользователь

--- VKRproject/timetable/views.py
+++ VKRproject/timetable/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import uuid
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Student
+from .serializer import StudentSerializer
+
+from django.http import JsonResponse
+import json
+
+@api_view(["GET"])
+def get_student_by_id(request):
+    student_id = request.GET['id']
+
+    student = Student.objects.get(id=student_id)
+
+    return JsonResponse({
+        'Surname': student.surname
+    }, status=200)
 
 # Create your views here.
