64a9e2079f57d5e87a2b8462a540a251345630aa
Мартюгов Сергей <ceregamart2003@gmail.com>
2023-05-20 12:55:19 +0300

    изменён пользователь

--- VKRproject/timetable/urls.py
+++ VKRproject/timetable/urls.py
@@ -3,12 +3,17 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('allstudents/', views.getAllStudents),
-
     path('student/add', views.add_student),
     path('teacher/add', views.add_teacher),
     path('group/add', views.add_group),
+    path('discipline/add', views.add_discipline),
+    path('audience/add', views.add_Classes_room),
+    path('phonesheadman/add', views.add_Student_Phones),
+    path('type_of_lessons/add', views.add_type_of_lessons),
+    path('students/update', views.update_students_data),
+    path('groups/update', views.update_groups_data),
     re_path(r'group/find/$', views.find_group_by_number),
-    re_path(r'student/find/$', views.find_student_by_id)
+    re_path(r'student/find/$', views.find_student_by_id),
+    re_path(r'headman/find/$', views.find_headman_by_number_of_group),
 
-        
 ]
--- VKRproject/timetable/views.py
+++ VKRproject/timetable/views.py
@@ -92,6 +92,19 @@ def find_group_by_number(request):
         'headman': group.id_headman
     })
 
+@api_view(['GET'])
+def find_headman_by_number_of_group(request):
+    groupnumb=request.GET["number"]
+    group=Group.objects.get(group_number=groupnumb)
+    headmanphones = request.GET["headman"]
+    Sphones=Student_Phones.objects.all()
+
+
+    return  JsonResponse({
+        'responce': "запрос прошёл",
+        'headman': group.id_headman
+    })
+
 @api_view(['POST'])
 def add_group(request):
     try:
@@ -108,10 +121,124 @@ def add_group(request):
             'id': group_id
         })
     except Exception as error:
-        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400) #узнать почему ошибка
-
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
 
     return Response()
+
+@api_view(['POST'])
+def add_discipline(request):
+    try:
+        data = json.loads(request.body.decode())
+        discipline_id = uuid.uuid4()
+
+        discipline.objects.create(
+            id = discipline_id,
+            name_of_discipline = data["name_of_discipline"],
+            department = data["department"],
+            )
+        return JsonResponse({
+            'id': discipline_id
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
+
+@api_view(['POST'])
+def add_Student_Phones(request):
+    try:
+        data = json.loads(request.body.decode())
+        Student_Phones_id = uuid.uuid4()
+
+        Student_Phones.objects.create(
+            id = Student_Phones_id,
+            phone = data["phone"],
+            id_student = data["id_student"],
+            )
+        return JsonResponse({
+            'id': Student_Phones_id
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
+
+@api_view(['POST'])
+def add_Classes_room(request):
+    try:
+        data = json.loads(request.body.decode())
+        Classes_room_id = uuid.uuid4()
+
+        Classes_room.objects.create(
+            id = Classes_room_id,
+            number = data["number"]
+            )
+        return JsonResponse({
+            'id': Classes_room_id
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
+@api_view(['POST'])
+def add_type_of_lessons(request):
+    try:
+        data = json.loads(request.body.decode())
+        type_of_lessons_id = uuid.uuid4()
+
+        type_of_lessons.objects.create(
+            id = type_of_lessons_id,
+            type_of_lessons = data["type_of_lessons"]
+            )
+        return JsonResponse({
+            'id': type_of_lessons_id
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
+
+
+@api_view(['POST'])
+def update_students_data(request):
+    try:
+        data = json.loads(request.body.decode())
+        student = Student.objects.get(id=data["id"])
+
+        student.surname = data["surname"]
+        student.name = data["name"]
+        student.patronic = data["patronic"]
+        student.id_group = data["id_group"]
+        student.student_book_number = data["student_book_number"]
+
+
+        student.save()
+        return JsonResponse({
+            'responce': "Данные студента успешно обновлены"
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
+
+@api_view(['POST'])
+def update_groups_data(request):
+    try:
+        data = json.loads(request.body.decode())
+        group = Group.objects.get(group_number=data["group_number"])
+
+        group.year_of_admission = data["year_of_admission"]
+        group.id_headman = data["id_headman"]
+        group.direction_information = data["direction_information"]
+
+        group.save()
+        return JsonResponse({
+            'responce': "Данные группы успешно обновлены"
+        })
+    except Exception as error:
+        return JsonResponse({'error': 'Произошла ошибка во время выполнения запроса'}, status = 400)
+    return Response()
+
 
 
 @api_view(['GET'])
