e38b600501fcede2de045694914882c48aedc315
Мартюгов Сергей <ceregamart2003@gmail.com>
2023-05-23 20:17:30 +0300

    Исправил тип данных у ID

--- VKRproject/timetable/models.py
+++ VKRproject/timetable/models.py
@@ -6,12 +6,12 @@ class Student(models.Model):
     surname = models.CharField(max_length=40, blank=False, default="")
     name = models.CharField(max_length=40, blank=False, default="")
     patronic = models.CharField(max_length=40, blank=False, default="", null=True)
-    id_group = models.CharField(max_length=40, blank=False, default="")
+    id_group = models.UUIDField()
     student_book_number = models.CharField(max_length=40, blank=False, default="")
 
 class Student_Phones(models.Model):
     phone = models.CharField(max_length=20, blank=False, default="")
-    id_student = models.CharField(max_length=50, blank=False, default="")
+    id_student = models.UUIDField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 class Teacher(models.Model):
@@ -22,24 +22,24 @@ class Teacher(models.Model):
 
 class Teacher_Phones(models.Model):
     phone = models.CharField(max_length=20, blank=False, default="")
-    id_teacher = models.CharField(max_length=50, blank=False, default="")
+    id_teacher = models.UUIDField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 
 class Classes_room(models.Model):
     number = models.CharField(max_length=20, blank=False, default="")
-    id_build = models.CharField(max_length=50, blank=False, default="")
+    id_build = models.UUIDField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 class email_teacher(models.Model):
     email = models.CharField(max_length=20, blank=False, default="")
-    id_teacher = models.CharField(max_length=50, blank=False, default="")
+    id_teacher = models.UUIDField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     year_of_admission = models.CharField(max_length=40, blank=False, default="")
-    id_headman = models.CharField(max_length=50, blank=False, default="")
+    id_headman = models.UUIDField()
     group_number = models.CharField(max_length=40, blank=False, default="", null=True)
     direction_information = models.CharField(max_length=40, blank=False, default="")
 
@@ -54,12 +54,12 @@ class type_of_lessons(models.Model):
 
 class Lesson(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    id_lesson_participants = models.CharField(max_length=50, blank=False, default="")
-    id_teacher = models.CharField(max_length=50, blank=False, default="")
-    id_discipline = models.CharField(max_length=50, blank=False, default="")
-    id_audience = models.CharField(max_length=50, blank=False, default="")
-    date_and_time = models.CharField(max_length=40, blank=False, default="")    #не уверен что тут именно так пишется тип данных
-    id_type_of_lesson = models.CharField(max_length=50, blank=False, default="")
+    id_lesson_participants = models.UUIDField()
+    id_teacher = models.UUIDField()
+    id_discipline = models.UUIDField()
+    id_audience = models.UUIDField()
+    date_and_time = models.UUIDField()
+    id_type_of_lesson = models.UUIDField()
 
 class Build(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
